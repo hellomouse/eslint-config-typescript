b3811c14f8f5745389e36ac63f13c178786b58c9
iczero <iczero4@gmail.com>
2019-08-10 16:32:53 -0700

    Turn off prefer-const rule, document choices Bump version to 1.0.2

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'eslint-config-google',
+    'eslint-config-google'
   ],
   rules: {
+    // highly subjective: dangling commas feel weird,
     'comma-dangle': ['error', 'never'],
+
+    // so you can do n => +n instead of (n) => +n
     'arrow-parens': ['error', 'as-needed'],
-    'indent': ['error', 2, { SwitchCase: 2 }],
+
+    // 2 spaces for indent
+    'indent': ['error', 2, { SwitchCase: 1 }],
+
+    // console.log() is often used in node.js
     'no-console': 'off',
+
+    // spaces after curly brackets look cleaner ({a: 4} vs { a: 4 })
+    // also done by util.inspect
     'object-curly-spacing': ['error', 'always'],
+
+    // a + b rather than a+b, but allow integer hints to not have spaces (eg. b|0)
     'space-infix-ops': ['error', { int32Hint: true }],
-    'max-len': ['error', 120],
-    'eqeqeq': ['error', 'always']
+
+    // lines should generally be kept short but definitely shouldn't be over 120 characters
+    // allow long strings and regexes though
+    'max-len': ['error', {
+      code: 120,
+      ignoreUrls: true,
+      ignoreStrings: true,
+      ignoreTemplateLiterals: true,
+      ignoreRegExpLiterals: true
+    }],
+
+    // == has weird behavior with types, always use === instead
+    'eqeqeq': ['error', 'always'],
+
+    // errors due to unintentional reassigning generally are quite rare, in addition,
+    // changing from const to let is a bit annoying
+    'prefer-const': 'off'
   }
 };
